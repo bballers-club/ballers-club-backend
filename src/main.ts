c8ac47c921877826ef7f1f17f95a16cd80eee9de
Nixo <tran.nicolas28@gmail.com>
2023-04-22 00:13:17 +0200

    Setting port in .env

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { config } from "dotenv"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  config()
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Ballers Club API')
     .setDescription('Welcome to the Ballers Club API documentation')
     .setVersion('1.0')
     .addTag('ballers club')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('ballers-club-api', app, document);
 
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
