a5823f655f1a64eeaf14909dd30c066ce63e3072
tnicolas28 <tran.nicolas28@gmail.com>
2023-05-17 12:06:20 +0200

    feat : returns id's on favorite delete

--- src/user-favorite-playground/providers/user-favorite-playground.service.ts
+++ src/user-favorite-playground/providers/user-favorite-playground.service.ts
@@ -5,30 +5,33 @@ import { z } from 'zod';
 
 @Injectable()
 export class UserFavoritePlaygroundService {
-  constructor(
+	constructor(
 		@Inject('USER_FAVORITE_PLAYGROUND_REPOSITORY')
 		private userFavoritePlaygroundRepository: Repository<UserFavoritePlayground>,
 	) {}
 
+	async create(createUserFavoritePlayground: {
+		userId: string;
+		playgroundId: string;
+	}): Promise<UserFavoritePlayground> {
+		try {
+			const validatedUserFavoritePlayground = z
+				.object({
+					userId: z.string().uuid(),
+					playgroundId: z.string().uuid(),
+				})
+				.parse(createUserFavoritePlayground);
 
-  async create(createUserFavoritePlayground: {
-    userId : string,
-    playgroundId : string
-  }) : Promise<UserFavoritePlayground> {
-    try{
-      const validatedUserFavoritePlayground = z.object({
-        userId : z.string().uuid(),
-        playgroundId : z.string().uuid()
-      }).parse(createUserFavoritePlayground);
+			const createdValidatedUserFavoritePlayground =
+				await this.userFavoritePlaygroundRepository.create({
+					...validatedUserFavoritePlayground,
+				});
 
-      const createdValidatedUserFavoritePlayground = await this.userFavoritePlaygroundRepository.create({
-        ...validatedUserFavoritePlayground
-      })
-
-      return await this.userFavoritePlaygroundRepository.save(createdValidatedUserFavoritePlayground)
-    }
-    catch(error){
-      throw new HttpException(
+			return await this.userFavoritePlaygroundRepository.save(
+				createdValidatedUserFavoritePlayground,
+			);
+		} catch (error) {
+			throw new HttpException(
 				{
 					status: HttpStatus.BAD_REQUEST,
 					error: `Invalid parameter : ${error.message}`,
@@ -38,22 +41,22 @@ export class UserFavoritePlaygroundService {
 					cause: error,
 				},
 			);
-    }
-  }
+		}
+	}
 
-  async findAllFavoriteOfUser(userId : string) : Promise<UserFavoritePlayground[]> {
-    try{
-      const validatedUserId = z.string().uuid().parse(userId)
+	async findAllFavoriteOfUser(
+		userId: string,
+	): Promise<UserFavoritePlayground[]> {
+		try {
+			const validatedUserId = z.string().uuid().parse(userId);
 
-      return await this.userFavoritePlaygroundRepository.find({
-        where : {
-          userId : validatedUserId
-        }
-      });
-
-    }
-    catch(error){
-      throw new HttpException(
+			return await this.userFavoritePlaygroundRepository.find({
+				where: {
+					userId: validatedUserId,
+				},
+			});
+		} catch (error) {
+			throw new HttpException(
 				{
 					status: HttpStatus.BAD_REQUEST,
 					error: `Invalid parameter : ${error.message}`,
@@ -63,22 +66,33 @@ export class UserFavoritePlaygroundService {
 					cause: error,
 				},
 			);
-    }
-  }
+		}
+	}
 
-  async removeFavorite(userId: String, playgroundId : string) : Promise<void> {
-    try{
-      const validatedUserId = z.string().uuid().parse(userId)
-      const validatedPlaygroundId = z.string().uuid().parse(playgroundId)
+	async removeFavorite(
+		userId: string,
+		playgroundId: string,
+	): Promise<Record<string, string>> {
+		try {
+			const validatedUserId = z.string().uuid().parse(userId);
+			const validatedPlaygroundId = z.string().uuid().parse(playgroundId);
 
-      await this.userFavoritePlaygroundRepository.createQueryBuilder()
-      .delete()
-      .from(UserFavoritePlayground)
-      .where("userId = :userId AND playgroundId = :playgroundId ", {userId : validatedUserId, playgroundId : validatedPlaygroundId} )
-      .execute()
-    }
-    catch(error){
-      throw new HttpException(
+			await this.userFavoritePlaygroundRepository
+				.createQueryBuilder()
+				.delete()
+				.from(UserFavoritePlayground)
+				.where('userId = :userId AND playgroundId = :playgroundId ', {
+					userId: validatedUserId,
+					playgroundId: validatedPlaygroundId,
+				})
+				.execute();
+
+			return {
+				userId: validatedUserId,
+				playgroundId: validatedPlaygroundId,
+			};
+		} catch (error) {
+			throw new HttpException(
 				{
 					status: HttpStatus.BAD_REQUEST,
 					error: `Invalid parameter : ${error.message}`,
@@ -88,6 +102,6 @@ export class UserFavoritePlaygroundService {
 					cause: error,
 				},
 			);
-    }
-  }
+		}
+	}
 }
--- src/user-favorite-playground/user-favorite-playground.controller.ts
+++ src/user-favorite-playground/user-favorite-playground.controller.ts
@@ -1,35 +1,60 @@
-import { Controller, Get, Post, Body, Param, Delete, Query } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Param,
+	Delete,
+	Query,
+} from '@nestjs/common';
 import { UserFavoritePlaygroundService } from './providers/user-favorite-playground.service';
 import { CreateUserFavoritePlaygroundDto } from './dto/create-user-favorite-playground.dto';
 import { ApiTags, ApiResponse, ApiQuery } from '@nestjs/swagger';
 import { UserFavoritePlaygroundDto } from './dto/user-favorite-playground.dto';
 import { UserFavoritePlayground } from './entity/user-favorite-playground.entity';
 
-
-@ApiTags("user-favorite-playground")
+@ApiTags('user-favorite-playground')
 @Controller('user-favorite-playground')
 export class UserFavoritePlaygroundController {
-  constructor(private readonly userFavoritePlaygroundService: UserFavoritePlaygroundService) {}
+	constructor(
+		private readonly userFavoritePlaygroundService: UserFavoritePlaygroundService,
+	) {}
 
-  @ApiResponse({
-    description : "Route to add favorite for a user",
-    type : UserFavoritePlaygroundDto
-  })
-  @Post()
-  async addFavorite(@Body() createUserFavoritePlaygroundDto: CreateUserFavoritePlaygroundDto) : Promise<UserFavoritePlayground>{
-    return await this.userFavoritePlaygroundService.create(createUserFavoritePlaygroundDto);
-  }
+	@ApiResponse({
+		description: 'Route to add favorite for a user',
+		type: UserFavoritePlaygroundDto,
+	})
+	@Post()
+	async addFavorite(
+		@Body()
+		createUserFavoritePlaygroundDto: CreateUserFavoritePlaygroundDto,
+	): Promise<UserFavoritePlayground> {
+		return await this.userFavoritePlaygroundService.create(
+			createUserFavoritePlaygroundDto,
+		);
+	}
 
-  @Get(':id')
-  async findUserFavorites(@Param('id') userId: string) : Promise<UserFavoritePlayground[]>{
-    return await this.userFavoritePlaygroundService.findAllFavoriteOfUser(userId);
-  }
+	@Get(':id')
+	async findUserFavorites(
+		@Param('id') userId: string,
+	): Promise<UserFavoritePlayground[]> {
+		return await this.userFavoritePlaygroundService.findAllFavoriteOfUser(
+			userId,
+		);
+	}
 
-  @ApiQuery({
-    example : "user-favorite-playground?userId=:userId&playgroundId=:playgroundId"
-  })
-  @Delete()
-  removeFavorite(@Query('userId') userId : string, @Query('playgroundId') playgroundId : string) {
-    return this.userFavoritePlaygroundService.removeFavorite(userId, playgroundId);
-  }
+	@ApiQuery({
+		example:
+			'user-favorite-playground?userId=:userId&playgroundId=:playgroundId',
+	})
+	@Delete()
+	removeFavorite(
+		@Query('userId') userId: string,
+		@Query('playgroundId') playgroundId: string,
+	): Promise<Record<string, string>> {
+		return this.userFavoritePlaygroundService.removeFavorite(
+			userId,
+			playgroundId,
+		);
+	}
 }
